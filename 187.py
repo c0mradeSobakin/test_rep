ad611e288f308cf3960dfe3fb56b1de03bc5de42
Sobakin <c0mrade.Sobakin@yandex.ru>
2022-03-20 17:05:25 +0300

    Update 187.py
    Converted ip to 32-bit system

--- 187.py
+++ 187.py
@@ -1,2 +1,9 @@
 def ip_to_int32(ip):
-  # your code here
+    list_octets = ip.split('.')
+    return (int(list_octets[0]) * 16777216) + \
+           (int(list_octets[1]) * 65536) + \
+           (int(list_octets[2]) * 256) + \
+            int(list_octets[3])
+
+
+ip_to_int32("128.114.17.104")
